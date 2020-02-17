c5c03a4c66b52dc2b9c502396ed43d206a5a67d7
hanhnus <hanh.nus@gmail.com>
2020-02-17 22:08:37 +1300

    Update AWS_Glue_ETL_script.py

--- AWS_Glue_ETL_script.py
+++ AWS_Glue_ETL_script.py
@@ -1,9 +1,9 @@
 import sys
 from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
+from awsglue.utils      import getResolvedOptions
+from pyspark.context    import SparkContext
+from awsglue.context    import GlueContext
+from awsglue.job        import Job
 
 ## @params: [JOB_NAME]
 args        = getResolvedOptions(sys.argv, ['JOB_NAME'])
